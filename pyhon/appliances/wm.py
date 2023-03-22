6c3da96a03ec06e7960c7e67e2a1bac792a5fd99
Andre Basche <andre.basche@gmail.com>
2023-03-22 22:42:40 +0100

    Fix not showing device active when running

--- pyhon/appliances/wm.py
+++ pyhon/appliances/wm.py
@@ -5,6 +5,6 @@ class Appliance:
     def get(self):
         if self._data["attributes"]["lastConnEvent"]["category"] == "DISCONNECTED":
             self._data["attributes"]["parameters"]["machMode"] = "0"
-        self._data["active"] = bool(self._data.get("activity"))
+        self._data["active"] = bool(self._data.get("attributes", {}).get("activity"))
         self._data["pause"] = self._data["attributes"]["parameters"]["machMode"] == "3"
         return self._data
