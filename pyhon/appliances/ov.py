18b0ecdd68ac8cd4cc8bae248883487ccb47d96d
Miguel Ángel <miguel.angel.lopez.vicente@gmail.com>
2023-04-08 22:24:14 +0200

    Override Oven attributes when is disconnected

--- pyhon/appliances/ov.py
+++ pyhon/appliances/ov.py
@@ -8,6 +8,12 @@ class Appliance:
         self._program_filter = HonParameterEnum("program_filter", data)
 
     def data(self, data):
+        if data["attributes"]["lastConnEvent"]["category"] == "DISCONNECTED":
+            data["attributes"]["activity"]["attributes"]["temp"] = "0"
+            data["attributes"]["activity"]["attributes"]["onOffStatus"] = "0"
+            data["attributes"]["activity"]["attributes"]["remoteCtrValid"] = "0"
+            data["attributes"]["activity"]["attributes"]["remainingTimeMM"] = "0"
+
         return data
 
     def settings(self, settings):
