411effd81483654bbc102afd2a25973c1cbe7c89
Andre Basche <andre.basche@gmail.com>
2023-04-17 00:37:19 +0200

    Fix disappearing programs

--- pyhon/parameter/program.py
+++ pyhon/parameter/program.py
@@ -1,4 +1,4 @@
-from typing import List, TYPE_CHECKING
+from typing import List, TYPE_CHECKING, Dict
 
 from pyhon.parameter.enum import HonParameterEnum
 
@@ -13,7 +13,7 @@ class HonParameterProgram(HonParameterEnum):
         super().__init__(key, {})
         self._command = command
         self._value: str = command.program
-        self._values: List[str] = list(command.programs)
+        self._programs: Dict[str, "HonCommand"] = command.programs
         self._typology: str = "enum"
 
     @property
@@ -25,9 +25,9 @@ class HonParameterProgram(HonParameterEnum):
         if value in self.values:
             self._command.program = value
         else:
-            raise ValueError(f"Allowed values {self._values}")
+            raise ValueError(f"Allowed values {self.values}")
 
     @property
     def values(self) -> List[str]:
-        values = [v for v in self._values if all(f not in v for f in self._FILTER)]
+        values = [v for v in self._programs if all(f not in v for f in self._FILTER)]
         return sorted(values)
