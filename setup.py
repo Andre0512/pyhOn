dfbc24452b350f64b9075d3948a9a0cc6df36cd1
Andre Basche <andre.basche@gmail.com>
2023-05-16 21:26:15 +0200

    Fix value check

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pyhOn",
-    version="0.10.7",
+    version="0.10.8",
     author="Andre Basche",
     description="Control hOn devices with python",
     long_description=long_description,
