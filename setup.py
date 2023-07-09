f1818bbc5d5489dd6ccbeba31cc9690e320c5121
Andre Basche <andre.basche@gmail.com>
2023-07-09 23:59:27 +0200

    Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="pyhOn",
-    version="0.14.7",
+    version="0.14.8",
     author="Andre Basche",
     description="Control hOn devices with python",
     long_description=long_description,
