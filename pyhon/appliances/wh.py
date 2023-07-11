a17692940b4c6fbfd9359dd8bfc438e81b61c470
Vadym Melnychuk <vme@primexm.com>
2023-07-11 18:38:49 +0300

    cleanup

--- pyhon/appliances/wh.py
+++ pyhon/appliances/wh.py
@@ -2,6 +2,7 @@ from typing import Any, Dict
 
 from pyhon.appliances.base import ApplianceBase
 
+
 class Appliance(ApplianceBase):
     def attributes(self, data: Dict[str, Any]) -> Dict[str, Any]:
         data = super().attributes(data)
