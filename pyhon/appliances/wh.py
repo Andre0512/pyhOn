a9fd05f3f5874d1fcbb2b5e38f06bd521ca9d9b1
Vadym Melnychuk <vme@primexm.com>
2023-07-11 17:40:04 +0300

    fixed build

--- pyhon/appliances/wh.py
+++ pyhon/appliances/wh.py
@@ -5,4 +5,6 @@ from pyhon.appliances.base import ApplianceBase
 class Appliance(ApplianceBase):
     def attributes(self, data: Dict[str, Any]) -> Dict[str, Any]:
         data = super().attributes(data)
-        data["active"] = data.get("onOffStatus") == 1
+        data["active"] = data["parameters"]["onOffStatus"] == "1"
+
+        return data
