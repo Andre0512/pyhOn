c71f8f17f566833586d540fd8a730a5280296453
Andre Basche <andre.basche@gmail.com>
2023-11-20 17:50:28 +0100

    Fix error in wh

--- pyhon/appliances/wh.py
+++ pyhon/appliances/wh.py
@@ -7,7 +7,7 @@ from pyhon.parameter.base import HonParameter
 class Appliance(ApplianceBase):
     def attributes(self, data: Dict[str, Any]) -> Dict[str, Any]:
         data = super().attributes(data)
-        parameter = data["parameters"]["onOffStatus"]
+        parameter = data.get("parameters", {}).get("onOffStatus")
         is_class = isinstance(parameter, HonParameter)
         data["active"] = parameter.value == 1 if is_class else parameter == 1
         return data
