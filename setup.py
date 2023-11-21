e9f2bb9f4fbfa37c072b1cccc547fc847c7fff23
Andre Basche <andre.basche@gmail.com>
2023-11-21 02:23:38 +0100

    Send program name Andre0512/hon#124

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pyhOn",
-    version="0.15.13",
+    version="0.15.14",
     author="Andre Basche",
     description="Control hOn devices with python",
     long_description=long_description,
