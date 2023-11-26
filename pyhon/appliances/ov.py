d4e57931863620595aa7a3c0848bde89c32a0485
Mguel Ángel <miguel.angel.lopez.vicente@gmail.com>
2023-11-26 13:36:41 +0100

    Fix check active oven

--- pyhon/appliances/ov.py
+++ pyhon/appliances/ov.py
@@ -7,10 +7,10 @@ class Appliance(ApplianceBase):
     def attributes(self, data: Dict[str, Any]) -> Dict[str, Any]:
         data = super().attributes(data)
         if data.get("lastConnEvent", {}).get("category", "") == "DISCONNECTED":
-            data["parameters"]["temp"].value = "0"
-            data["parameters"]["onOffStatus"].value = "0"
-            data["parameters"]["remoteCtrValid"].value = "0"
-            data["parameters"]["remainingTimeMM"].value = "0"
+            data["parameters"]["temp"].value = 0
+            data["parameters"]["onOffStatus"].value = 0
+            data["parameters"]["remoteCtrValid"].value = 0
+            data["parameters"]["remainingTimeMM"].value = 0
 
-        data["active"] = data["parameters"]["onOffStatus"] == "1"
+        data["active"] = data["parameters"]["onOffStatus"].value == 1
         return data
