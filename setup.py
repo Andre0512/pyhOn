4dc60c29ee07a83031d78ba9216b213c54757519
Andre Basche <andre.basche@gmail.com>
2024-01-11 01:18:10 +0100

    Fix crash in loading attributs Andre0512/hon#134

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="pyhOn",
-    version="0.15.14",
+    version="0.15.15",
     author="Andre Basche",
     description="Control hOn devices with python",
     long_description=long_description,
